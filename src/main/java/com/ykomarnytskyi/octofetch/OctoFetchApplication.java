8e67bc5052d33aa990c84e4ed43a0f4bd4bc405d
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-05-18 22:29:56 +0200

    renamed package, removed OctoFetch suffix

--- src/main/java/com/ykomarnytskyi/octofetch/OctoFetchApplication.java
+++ src/main/java/com/ykomarnytskyi/octofetch/OctoFetchApplication.java
@@ -1,4 +1,4 @@
-package com.ykomarnytskyi.octofetch.OctoFetch;
+package com.ykomarnytskyi.octofetch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
