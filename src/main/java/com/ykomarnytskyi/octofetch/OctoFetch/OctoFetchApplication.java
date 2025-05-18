32c2943f51c5dad77ccc40dc029e6f179c255680
Yurii Komarnytskyi <ykomarnytskyi2022@gmail.com>
2025-05-18 19:52:31 +0200

    initial commit

---
+++ src/main/java/com/ykomarnytskyi/octofetch/OctoFetch/OctoFetchApplication.java
@@ -0,0 +1,13 @@
+package com.ykomarnytskyi.octofetch.OctoFetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctoFetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctoFetchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ykomarnytskyi/octofetch/OctoFetch/OctoFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykomarnytskyi.octofetch.OctoFetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctoFetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
